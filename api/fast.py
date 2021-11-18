18cbef24c7ee5af5106c62da04854b0083f5fd73
Etienne Doumazane <etienne.doumazane@gmail.com>
2021-11-18 12:42:01 +0100

    1st commit

---
+++ api/fast.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+def index(pickup_datetime, pickup_longitude, pickup_latitude,
+          dropoff_longitude, dropoff_latitude, passenger_count):
+
+    return {
+        "pickup_datetime": pickup_datetime,
+        "pickup_longitude": pickup_longitude,
+        "pickup_latitude": pickup_latitude,
+        "dropoff_longitude": dropoff_longitude,
+        "dropoff_latitude": dropoff_latitude,
+        "passenger_count": passenger_count
+    }
+
+if __name__ == "__main__":
+    print(index(1,1,1,1,1,1))
